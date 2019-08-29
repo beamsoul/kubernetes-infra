656e522f2936650a3c234a48dae7aabc7344c275
beamsoul <nurzhanasamidinova@gmail.com>
2019-08-28 22:16:25 -0700

    add

--- route_table.tf
+++ route_table.tf
@@ -41,3 +41,32 @@ resource "aws_route_table" "private-us-west-2c-kubernetesbrian-com" {
     "kubernetes.io/kops/role"                   = "private-us-west-2c"
   }
 }
+resource "aws_route_table_association" "private-us-west-2a-kubernetesbrian-com" {
+  subnet_id      = "${aws_subnet.us-west-2a-kubernetesbrian-com.id}"
+  route_table_id = "${aws_route_table.private-us-west-2a-kubernetesbrian-com.id}"
+}
+
+resource "aws_route_table_association" "private-us-west-2b-kubernetesbrian-com" {
+  subnet_id      = "${aws_subnet.us-west-2b-kubernetesbrian-com.id}"
+  route_table_id = "${aws_route_table.private-us-west-2b-kubernetesbrian-com.id}"
+}
+
+resource "aws_route_table_association" "private-us-west-2c-kubernetesbrian-com" {
+  subnet_id      = "${aws_subnet.us-west-2c-kubernetesbrian-com.id}"
+  route_table_id = "${aws_route_table.private-us-west-2c-kubernetesbrian-com.id}"
+}
+
+resource "aws_route_table_association" "utility-us-west-2a-kubernetesbrian-com" {
+  subnet_id      = "${aws_subnet.utility-us-west-2a-kubernetesbrian-com.id}"
+  route_table_id = "${aws_route_table.kubernetesbrian-com.id}"
+}
+
+resource "aws_route_table_association" "utility-us-west-2b-kubernetesbrian-com" {
+  subnet_id      = "${aws_subnet.utility-us-west-2b-kubernetesbrian-com.id}"
+  route_table_id = "${aws_route_table.kubernetesbrian-com.id}"
+}
+
+resource "aws_route_table_association" "utility-us-west-2c-kubernetesbrian-com" {
+  subnet_id      = "${aws_subnet.utility-us-west-2c-kubernetesbrian-com.id}"
+  route_table_id = "${aws_route_table.kubernetesbrian-com.id}"
+}
